2a153d2f4d68d18f1c7ebcc0f915e04a248e6716
Greg Divis <gdivis@inedo.com>
2024-10-17 16:16:14 -0400

    update to sdk 3.1

--- Azure/InedoExtension/Properties/AssemblyInfo.cs
+++ Azure/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using Inedo.Extensibility;
 
 [assembly: AssemblyTitle("Azure")]
 [assembly: AssemblyDescription("Contains a ProGet Package Store backed by Azure and operations for working with Azure in BuildMaster and Otter.")]
@@ -13,6 +12,6 @@ using Inedo.Extensibility;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.0.2")]
-[assembly: AssemblyFileVersion("3.0.2")]
-[assembly: AppliesTo(InedoProduct.ProGet | InedoProduct.BuildMaster| InedoProduct.Otter)] 
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AppliesTo(InedoProduct.ProGet | InedoProduct.BuildMaster | InedoProduct.Otter)]
